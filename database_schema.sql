e884270ecc73da1fa3d1ad1986f27e4594f3a043
Li <luo.li086@gmail.com>
2025-09-29 22:08:50 +0200

    Adjust database schema

--- database_schema.sql
+++ database_schema.sql
@@ -1,11 +1,22 @@
-CREATE TABLE VIDEOS(
-video_id TEXT PRIMARY KEY,
-title TEXT,
-published_at TIMESTAMP,
-views INTEGER,
-likes INTEGER,
-comments INTEGER,
-tags TEXT,
-duration_seconds INTEGER,
-playlist_title TEXT
-)
+CREATE TABLE channels (
+    channel_id VARCHAR PRIMARY KEY,
+    channel_name TEXT NOT NULL,
+    description TEXT,
+    subscribers INT
+    views INT,
+    total_videos INT,
+    channel_playlist_id VARCHAR    
+);
+
+CREATE TABLE videos (
+    video_id VARCHAR PRIMARY KEY,    
+    title TEXT NOT NULL,
+    published_at TIMESTAMP,
+    views INT,
+    likes INT,
+    comments INT,
+    tags TEXT[],
+    channel_id VARCHAR NOT NULL,
+    duration_seconds INTERVAL,
+    FOREIGN KEY (channel_id) REFERENCES channels(channel_id)
+);
